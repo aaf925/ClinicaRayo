c2123813e529dfc87d9df5640a818fde9acb27a7
javisan2711 <javisan2711@gmail.com>
2024-12-17 11:31:30 +0100

    tienda

---
+++ controlador/tienda.php
@@ -0,0 +1,6 @@
+<?php
+require_once '../vista/menuUsuarioNoRegistrado.html';
+require_once '../vista/tienda.html';
+echo "<div style='margin-top: 140px;'></div>";
+require_once '../vista/piePagina.html';
+?>
